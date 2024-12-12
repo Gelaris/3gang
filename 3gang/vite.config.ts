1ae25cde0fd1c67a88ed723336c257adf0f8755b
Gelaris <Gelaris0@yandex.ru>
2024-12-13 02:50:58 +0300

    7ver

--- 3gang/vite.config.ts
+++ 3gang/vite.config.ts
@@ -4,12 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 3000
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
 })
